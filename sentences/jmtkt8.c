3b1604741cc0c4f4c547f49d7c913350ea95e9d5
jmtkt8 <muitprogram@mwc-037054.dhcp.missouri.edu>
2015-09-01 13:19:30 -0500

    Made Changes/Attempt 1

---
+++ sentences/jmtkt8.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int jmtkt8Sentence1()
+{
+  printf("Because toasters ain't loyal!\n");
+  return 1;
+}
+
+int jmtkt8Sentence2()
+{
+  printf("And dropped the hottest mixtape of 2015. \n");
+  return 1;
+}
