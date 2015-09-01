118dc4683e3a6a3f3c8c83323a3591f86c954297
cwsvf2 <cwsvf2@mail.missouri.edu>
2015-09-01 13:03:16 -0500

    adjusted sentences

--- sentences/cwsvf2.c
+++ sentences/cwsvf2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int cwsvf2Sentence1(){
-	printf("\nHe tried to escape, but the King Cheetah was too fast.");
+	printf("\nHe was unable to escape, the King Cheetah was too fast.");
 	return 1;
 }
 
 int cwsvf2Sentence2(){
-	printf("\nOnly a visit from THE CLOUD can heal a wounded unicorn.");
+	printf("\nNot even a visit from THE CLOUD can heal a wounded unicorn.");
 	return 1;
 }
