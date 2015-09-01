e21763b6a306abd5c79a428d732d305274a89e13
Piper Williams <pswnr9@mail.missouri.edu>
2015-08-31 19:26:23 -0500

    Create pswnr9.c

---
+++ sentences/pswnr9.c
@@ -0,0 +1,14 @@
+//pswnr9.c
+//Piper Williams 8/31/15
+
+#include <stdio.h>
+
+int pswnr9Sentence1(){
+	printf("He decided that he was only going to eat half of the big macs and give the other half to his dog.\n");
+	return 1;
+}
+
+int pswnr9Sentence2(){
+	printf("");
+	return 1;
+}
