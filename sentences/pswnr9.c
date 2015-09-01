63385b7183002a2ccb7d4636a7f2ce68210ae2fb
Piper Williams <pswnr9@mail.missouri.edu>
2015-09-01 14:04:37 -0500

    Update pswnr9.c

--- sentences/pswnr9.c
+++ sentences/pswnr9.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 
 int pswnr9Sentence1(){
-	printf("\n He decided that he was only going to eat half of the big macs and give the other half to his ninja friend.\n");
+	printf("\n The wizard decided that he was only going to eat half of the big macs and give the other half to his ninja friend.\n");
 	return 1;
 }
 
 int pswnr9Sentence2(){
-	printf("\nWho doesnt like to go bowling Warlocks?\n");
+	printf("\nWho doesnt like to go bowling Chupacabra?\n");
 	return 1;
 }
