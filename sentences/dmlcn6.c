681b94efb94d422f41729db3beb3f54b343b8c27
dmlcn6 <dmlcn6@mail.missouri.edu>
2015-09-03 21:44:03 -0500

    add dmlcn6.c

---
+++ sentences/dmlcn6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int dmlcn6Sentence1(){
+	printf("\nthe wizard managed to shapeshift into a merman, which saved his life.\n");
+	return 1;
+}
+
+int dmlcn6Sentence2(){
+	printf("And on the last day of that trip, Linus convinced the wizard into using his powers to make a robo-cryptid zoo, then they\n");
+	return 1;
+}
