d46c11a946aa03c9aa7a8f30031a2ecf44215b3b
kgh458 <karolinahryszko@Karolinas-MacBook-Pro.local>
2015-09-05 16:58:08 -0500

    Revert "Revert "fixed issues""
    This reverts commit a70486e47c57bcff9547a6fa438ef708309de679.

---
+++ SWE-F15/sentences/amcqb4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int amcqb4Sentence1()
+{
+  printf("\nThe Wizards learned how to hack into 'the cloud.'\n");
+  return 1;
+}
+int amcqb4Sentence2()
+{
+  printf("\nThey were successful at disabling the Ninja's Samurai Swords.\n");
+  return 1;
+}
---
+++ SWE-F15/sentences/mbs9b7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int mbs9b7Sentence1()
+{
+	printf("\nThe Wizard was convinced he could take John Cena. \n");
+	return 1;
+}
+
+int mbs9b7Sentence2()
+{
+	printf("\nThe Wizard was furious!\n");
+	return 1;
+}
