f1b6f14828bd9c7bf6d4f4a54d0453039287f348
kgh458 <karolinahryszko@Karolinas-MacBook-Pro.local>
2015-09-05 16:57:10 -0500

    Revert "fixed issues"
    This reverts commit b3833bf9d0f0bcf24c72a344b2921fc13aa353bd.

--- story.c
+++ story.c
@@ -1,3 +1,317 @@
+<<<<<<< HEAD
+#include <stdio.h>
+#include "sentences/_HEADERS.h"
+
+// I assume all of you know C.
+
+int main( void )
+{
+	printf("\n\n");
+	bensSentence1();
+	printf("\n\n");
+
+	// Add your sentence.
+	// Don't forget to update the header file.
+	// vvvvvvvvvvvvvvvv STUDENT WORK HERE
+
+	cjc455Sentence1();
+	printf("\n\n");
+	jbekccSentence1();    
+	printf("\n\n");
+
+	dhart94Sentence1();    
+	printf("\n\n");      
+	
+	jkdt2Sentence1();    
+	printf("\n\n");
+	
+	cjc455Sentence2();
+	printf("\n\n");
+
+	jrf5x8Sentence1();
+	printf("\n");  
+		
+	ampwd6Sentence1();
+	printf("\n\n");
+
+	bdfd63Sentence1();
+	printf("\n\n");
+
+    mdvy96Sentence1();
+   	printf("\n\n");
+
+   	mbs9b7Sentence1();
+   	printf("\n\n");
+
+   	anmg8Sentence2();
+	printf("\n\n");
+
+	//jtr4bdSentence1();
+	//printf("\n\n");
+    
+    jlhthdSentence1();
+    printf("\n\n");
+
+	cwm262Sentence1();
+	printf("\n\n");
+
+	dmlcn6Sentence1();
+	
+	ajsvt7Sentence1();
+	printf("\n\n");
+ 
+	essxpdSentence1();
+	printf("\n\n");
+
+	skh6yfSentence1();
+	printf("\n\n");
+	
+	pswnr9Sentence1();
+	printf("\n\n");
+
+	njmnbbSentence1();
+	printf("\n\n");
+	
+	tmb4qfSentence1();
+
+	printf("\n\n");
+
+	dbmfzfSentence1();
+	printf("\n");
+
+	printf("\n\n");
+
+	cmw72dSentence1();
+	printf("\n\n");
+
+	mjwpq6Sentence1();
+
+	printf("\n\n"); 
+	
+	frieljSentence1();
+	printf("\n\n");
+
+    jrocncSentence1();
+    printf("\n\n");
+	
+	ccyd6cSentence1();
+	printf("\n\n");
+
+	bdfd63Sentence2();
+	printf("\n\n");
+
+	ajmhydSentence1();
+	printf("\n");
+    
+    jmtkt8Sentence2();
+    printf("\n");
+
+    drrkvfSentence1();
+    printf("\n\n");
+ 
+    rcn6f4Sentence1();
+    printf("\n\n");
+
+    mbs9b7Sentence2();
+    printf("\n\n");
+
+    anmg8Sentence1();
+	printf("\n\n");
+
+	drrkvfSentence2();
+	printf("\n\n");
+
+	cwsvf2Sentence1();
+	printf("\n\n");
+	
+	bsb62Sentence1();
+	printf("\n\n");
+	
+	// acbgf8Sentence1();
+	// printf("\n\n");
+	cde9d5Sentence1();
+	printf("\n\n");
+
+	dhart94Sentence2();
+	printf("\n\n");    
+	
+	skh6yfSentence2();
+    printf("\n\n");
+
+	essxpdSentence2();
+	printf("\n\n");
+
+
+	jrf5x8Sentence2();
+	printf("\n");
+
+	cwcf34Sentence1();
+	printf("\n\n");
+
+	aaan94Sentence1();
+	printf("\n");
+	
+	dmlcn6Sentence2();
+
+	njmnbbSentence2();
+	printf("\n\n");
+
+	ampwd6Sentence2();
+	printf("\n\n"); 
+	
+	jkdt2Sentence2();   
+	printf("\n\n");
+    
+    mdvy96Sentence2();
+    printf("\n\n");
+    
+    jlhthdSentence2();
+    printf("\n\n");
+
+	cwm262Sentence2();
+	printf("\n\n");
+	
+	jbekccSentence2();
+	printf("\n\n");
+
+	crmmx2Sentence1();
+	printf("\n\n");
+	
+	tmb4qfSentence2();
+	printf("\n\n");
+    
+    jmtkt8Sentence1();
+    printf("\n");
+	
+	aaan94Sentence2();
+    	printf("\n");
+    
+	frieljSentence2();
+	printf("\n\n");
+	
+	ajsvt7Sentence2();
+	printf("\n\n");
+
+    jrocncSentence2();
+    printf("\n\n");
+	
+	pswnr9Sentence2();
+	printf("\n\n");
+
+	cwsvf2Sentence2();
+	printf("\n\n");
+	
+	mcbxx2Sentence1();
+	mcbxx2Sentence2();
+	printf("\n\n");
+
+	//kjbr3dSentence1();	
+	//printf("\n\n");
+
+	mjk8v2Sent1();
+	mjk8v2Sent2();
+	mjk8v2Sent3();
+	printf("\n\n");
+
+	mjwpq6Sentence2();
+	printf("\n\n");
+
+	//jtr4bdSentence2();
+	//printf("\n\n");
+
+    rcn6f4Sentence2();
+	printf("\n\n");
+
+	ccyd6cSentence2();
+	printf("\n\n");
+
+
+	// acbgf8Sentence2();
+	// printf("\n\n");
+
+	crmmx2Sentence2();
+	printf("\n\n");
+
+	cmw72dSentence2();
+	printf("\n\n");
+
+	ajmhydSentence2();
+	printf("\n");
+
+	dbmfzfSentence2();
+	printf("\n");
+	
+	bsb62Sentence2();
+	printf("\n");
+	bsb62Sentence3();
+	printf("\n");
+	
+	cwcf34Sentence2();
+	printf("\n\n");
+	
+	cde9d5Sentence2();
+	printf("\n\n");
+
+	msr5zbSentence1();
+	printf("\n\n");
+	
+	//kjbr3dSentence2();
+	//printf("\n\n");
+
+	//djs6g7Sentence1();
+	//printf("\n\n");
+
+	//djs6g7Sentence2();
+        //printf("\n\n");
+
+	mttfdSentence1();
+	printf("\n\n");
+	
+	mttfdSentence2();
+        printf("\n\n");
+	
+	twn64Sentence1();
+	printf("\n\n");
+
+	twn64Sentence2();
+	printf("\n\n");
+
+	mjb6z9Sentence1();
+        printf("\n\n");
+
+	mjb6z9Sentence2();
+        printf("\n\n");
+
+	msr5zbSentence2();
+        printf("\n\n");
+		
+	dec5x8Sentense1();
+		printf("\n\n");
+		
+	dec5x8Sentense2();
+		printf("\n\n");
+    
+    amcqb4Sentence1();
+    printf("\n\n");
+    
+    amcqb4Sentence2();
+    printf("\n\n");
+	
+	afgd54Sentence1();
+	printf("\n\n");
+
+	afgd54Sentence2();
+	printf("\n\n");
+	
+	// DO NOT ADD AFTER THIS COMMENT
+	// Bens sentence should be last.
+	bensSentence_FINAL();
+	printf("\n\n");
+}
+
+
+
+=======
 #include <stdio.h>
 #include "sentences/_HEADERS.h"
 
@@ -296,11 +610,11 @@ int main( void )
 	msr5zbSentence2();
         printf("\n\n");
 		
-//	dec5x8Sentence1();
-//		printf("\n\n");
+	dec5x8Sentence1();
+		printf("\n\n");
 		
-//	dec5x8Sentence2();
-//		printf("\n\n");
+	dec5x8Sentence2();
+		printf("\n\n");
     
     amcqb4Sentence1();
     printf("\n\n");
@@ -328,3 +642,4 @@ int main( void )
 
 
 
+>>>>>>> dc133316d6050b648797cbe978fca30fb19e557b
